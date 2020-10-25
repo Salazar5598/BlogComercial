384d3ab878396e2ee02c874bbc284ce2262d5c47
Carlos Salazar <carlossalazards@gmail.com>
2020-10-24 23:41:38 -0600

    agregando diseño dinamico

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 # Create your views here.
@@ -6,5 +6,9 @@ def post_list(request):
     posts = Post.objects.filter(
         fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
     
         
